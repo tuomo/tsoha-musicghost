556741d12aba5d0848fd9c3d31e9baf5df7ca3bf
Tuomo Lempiäinen <tuomo@lempiainen.net>
2011-04-18 14:51:33 +0300

    Added info.php

---
+++ app/actions/info.php
@@ -0,0 +1,9 @@
+<?php
+
+$_id = A('request/id');
+
+$_stm = A('db:SELECT r.title, a.name, r.first_year, r.type, r.format, '.
+          'r.annotation FROM artist a, record r WHERE a.id = r.artist '.
+          'AND r.id = ? ', array($_id));
+
+$record = $_stm->fetch();
