c0258c8a2c3f0fe5eb38d9a8337efe1cb8c10856
Tuomo Lempiäinen <tuomo@lempiainen.net>
2011-04-18 14:45:58 +0300

    Created links to record info pages

--- app/actions/index.php
+++ app/actions/index.php
@@ -1,4 +1,13 @@
 <?php
 
-$records = A('db:SELECT a.name, r.title, r.first_year, r.format '.
-             'FROM artist a, record r WHERE a.id = r.artist');
+$_stm = A('db:SELECT a.name, r.id, r.title, r.first_year, r.format '.
+          'FROM artist a, record r WHERE a.id = r.artist');
+
+$records = $_stm->fetchAll();
+
+foreach ($records as &$record) {
+    $id = $record['id'];
+    $url = Atomik::url('@info', array('id' => $id));
+    $record['url'] = $url;
+}
+unset($record);
