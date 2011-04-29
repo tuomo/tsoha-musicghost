33f4716f2c618e410afc9fd4d6e4390dc40d4e09
Tuomo Lempiäinen <tuomo@lempiainen.net>
2011-04-29 19:22:17 +0300

    Updated the add page

--- app/actions/add.php
+++ app/actions/add.php
@@ -1,3 +1,17 @@
 <?php
 
+require('init.php');
+
+requireLoggedIn();
+
+$artists = getDbColumn($_db, 'name', 'artist', 'sortname');
+
+$types = array(
+    'album', 'single', 'EP', 'compilation',
+    'soundtrack', 'interview', 'live', 'other'
+);
+
+$formats = getDbColumn($_db, 'name', 'format');
+$packagings = getDbColumn($_db, 'name', 'packaging');
+
 ?>
