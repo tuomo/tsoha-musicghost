d6fdaef5199803708348e0c3974d3cac4ffbab78
Tuomo Lempiäinen <tuomo@lempiainen.net>
2011-04-29 23:51:54 +0300

    Updated login.post.php

--- app/actions/login.post.php
+++ app/actions/login.post.php
@@ -1,10 +1,12 @@
 <?php
 
+require('init.php');
+
 if (isset($_POST['submit']))
 {
     $password = isset($_POST['password']) ? $_POST['password'] : '';
 
-    if ($password == A('password')) {
+    if ($password == A('config/password')) {
         Atomik::set('session/logged_in', true);
         Atomik::redirect('index');
     }
