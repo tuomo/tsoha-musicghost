871589fa0cfb67e9181171cfb64fc4259a0ae06b
Tuomo Lempiäinen <tuomo@lempiainen.net>
2011-04-29 22:51:26 +0300

    Fixed two errors in add.php

--- app/actions/add.php
+++ app/actions/add.php
@@ -8,13 +8,14 @@ $artists = $_db->query('SELECT id, name FROM artist ORDER BY sortname')
     ->fetchAll();
 
 $_boxsets = $_db->query(
-    'SELECT a.id, a.name, r.id, r.title FROM artist a, record r '.
-    'WHERE a.id = r.artist ORDER BY a.sortname, r.title'
+    'SELECT a.id AS a_id, a.name, r.id AS r_id, r.title '.
+    'FROM artist a, record r WHERE r.box_set IS TRUE AND a.id = r.artist '.
+    'ORDER BY a.sortname, r.title'
 )->fetchAll();
 $boxsets = array();
 foreach ($_boxsets as $_bs) {
-    $_id = $_bs['a.id'].'#'.$_bs['r.id'];
-    $_name = $_bs['a.name'].' - '.$_bs['r.title'];
+    $_id = $_bs['a_id'].'#'.$_bs['r_id'];
+    $_name = $_bs['name'].' - '.$_bs['title'];
     $boxsets[] = array('id' => $_id, 'name' => $_name);
 }
 
