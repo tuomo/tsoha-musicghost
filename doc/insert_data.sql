1b68b5afdafbce8dee8e6a3fcf7ad6e6c85051ce
Tuomo Lempiäinen <tuomo@lempiainen.net>
2011-04-29 22:44:31 +0300

    Updated insert_data.sql to reflect the new DB structure

--- doc/insert_data.sql
+++ doc/insert_data.sql
@@ -1,6 +1,3 @@
-INSERT INTO list (name, public) VALUES ('Collection', TRUE);
-INSERT INTO list (name, public) VALUES ('Shopping list', FALSE);
-
 INSERT INTO artist (name, sortname, homepage, annotation)
     VALUES ('Artist 1', 'Artist 1', 'http://artist1.com', 'aöfjfkfjkfk');
 INSERT INTO artist (name, sortname, homepage, annotation)
@@ -8,47 +5,40 @@ INSERT INTO artist (name, sortname, homepage, annotation)
 INSERT INTO artist (name, sortname, homepage, annotation)
     VALUES ('Foo Bar', 'Bar, Foo', 'http://asdf.com', 'requiöasfjköfk');
 
-INSERT INTO format (name) VALUES ('CD'), ('2CD'), ('DVD'), ('CD+DVD');
-
-INSERT INTO packaging (name) VALUES ('Jewel case'), ('Digipack'), ('Slimcase');
-
 INSERT INTO label (name, homepage, annotation)
     VALUES ('Label 1', 'http://label1.com', 'asjadfkaöfkakl');
 
 INSERT INTO record (
     artist,
     title,
-    box_status,
+    box_set,
     type,
     first_year,
     this_year,
     format,
     packaging,
-    label,
     lent
 ) VALUES (
     1,
     'Album 1',
-    'normal',
-    'album',
+    TRUE,
+    'Album',
     2000,
     2002,
     'CD',
     'Jewel case',
-    1,
     FALSE
 ), (
     2,
-    'Album 2',
-    'normal',
-    'album',
-    2010,
-    2010,
-    'CD+DVD',
-    'Digipack',
-    1,
+    'Album 1',
+    TRUE,
+    'Single',
+    2000,
+    2002,
+    'CD',
+    'Jewel case',
     FALSE
 );
 
-INSERT INTO record_list (record, list) VALUES (1, 1);
-INSERT INTO record_list (record, list) VALUES (2, 1);
+INSERT INTO record_list (record, list) VALUES (4, 'Default');
+INSERT INTO record_list (record, list) VALUES (5, 'Default');
