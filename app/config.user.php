9a050672aeb8b7b5949a5d71eeec01ebd95b25bc
Tuomo Lempiäinen <tuomo@lempiainen.net>
2011-04-29 17:35:14 +0300

    Added config.user.php for user modifiable settings

---
+++ app/config.user.php
@@ -0,0 +1,21 @@
+<?php
+
+Atomik::set('config', array(
+
+    // The name that is shown on the web site.
+    'name' => 'Example',
+
+    // The password that is needed to modify the collection.
+    'password' => 'example',
+
+    // The information that is needed to connect to the database.
+    'db' => array(
+        'host' => 'localhost',
+        'user' => 'tuomo',
+        'pass' => '',
+        'dbname' => 'musicghost'
+    )
+
+));
+
+?>
