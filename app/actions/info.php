668c2565e29550e1b128ff803522a228bb709810
Hanna Nieminen <hanna.m.nieminen@helsinki.fi>
2011-05-07 17:55:11 +0300

    Updated info page and css file

--- app/actions/info.php
+++ app/actions/info.php
@@ -7,7 +7,9 @@ $_id = A('request/id');
 $record = $_db->query(
     'SELECT a.name AS artist, l.name AS label, r.title, r.box_set, r.box_id, r.type, '.
     'r.first_year, r.this_year, r.format, r.packaging, r.limited, '.
-    'r.ltd_num, r.added, r.lent, r.borrower, r.annotation '.
+    'r.ltd_num, r.added, r.lent, r.borrower, r.annotation, '.
+    'a.homepage AS artist_homepage, a.annotation AS artist_annotation, '.
+    'l.homepage AS label_homepage, l.annotation AS label_annotation '.
     'FROM artist a, record r LEFT OUTER JOIN label l ON (l.id = r.label) '.
     'WHERE a.id = r.artist AND r.id = ?',
     array($_id)
