6d52545a9b4c623ea42b665078b1cd148f1ff1d6
Tuomo Lempiäinen <tuomo@lempiainen.net>
2011-05-07 22:01:01 +0300

    Added helper for printing option tags

---
+++ app/helpers/options.php
@@ -0,0 +1,14 @@
+<?php
+
+function options($options, $selected_id)
+{
+    $str = '';
+    foreach ($options as $o) {
+        $str .= "<option value=\"{$o['id']}\"";
+        if ($selected_id === $o['id']) $str .= ' selected="selected"';
+        $str .= ">{$o['name']}</option>\n";
+    }
+    return $str;
+}
+
+?>
