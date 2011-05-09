cc8b382780024f9c841eb5a183499a590a84af18
Hanna Nieminen <hanna.m.nieminen@helsinki.fi>
2011-05-09 09:34:29 +0300

    Edited error message style

--- app/helpers/errors.php
+++ app/helpers/errors.php
@@ -7,7 +7,7 @@ function errors()
     if (count($errors) > 0) {
         $str .= '<p>The following errors were detected:</p><ul>';
         foreach ($errors as $msg) {
-            $str .= "<li>$msg</li>";
+            $str .= "<li class=\"errorlist\">$msg</li>";
         }
         $str .= '</ul>';
     }
